253309a5c7a3fc79a4c31621bcff7e7f73eaf835
Emil-Ka <emilkaraev55@gmail.com>
2022-05-03 20:27:12 +0400

    bag 1.0

--- add_knowledge.php
+++ add_knowledge.php
@@ -1,14 +1,15 @@
 <?php
-$host = "std-mysql";
-$user = "std_1830_hashtag";
-$pass = "rootroot";
-$mysql = new mysqli($host, $user, $pass, "field");
-$result = $mysql->query("SELECT * FROM `field`");
-$result = $result->fetch_assoc();
+//$host = "std-mysql";
+// $user = "std_1830_hashtag";
+// $pass = "rootroot";
+// $mysql = new mysqli($host, $user, $pass, "field");
+// $result = $mysql->query("SELECT * FROM `field`");
+// $result = $result->fetch_assoc();
 
-if (!$result) {
-  echo 'err';
-}
+// if (!$result) {
+//   echo 'err';
+// }
+$result = 'abc';
 
 echo $result;
 ?> 
